b218205c91d0dac54c6eabbbcdc47d97b99d14ed
Preston Whitehall <pmwhitehall@gmail.com>
2019-08-29 13:30:05 -0700

    Initial implementation of component with hard-coded data

--- client/index.jsx
+++ client/index.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PhotoCarousel from './photoCarousel';
 
-const App = (props) => (
-  <div>
-    <h1>Hello World</h1>
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <PhotoCarousel />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
   module: {
     rules: [
       {
